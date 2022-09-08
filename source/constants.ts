6b026323b15161beb90d3cc1bda742ddd0fa8719
Ilya Pogrebensky <ilya@billboy.co.il>
2022-09-08 18:40:18 +0300

    NP-6: Happy flow works

--- source/constants.ts
+++ source/constants.ts
@@ -10,6 +10,9 @@ export class ErrorMessages {
 
 export class Queries {
     public static WhiteBoardTypes: string = "SELECT * FROM white_board_type";
+
+    // SELECT * FROM white_board_type WHERE id =  50
+    public static WhiteBoardTypeById: string = "SELECT * FROM white_board_type WHERE id = ";
 }
 
 export const DB_CONNECTION_STRING: string = "server=.;Database=masa_school;Trusted_Connection=Yes;Driver={SQL Server Native Client 11.0}";
