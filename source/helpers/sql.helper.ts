4def4156895124e3b26a7b071c3c820a601d25da
Ilya Pogrebensky <ilya@billboy.co.il>
2022-09-11 12:59:38 +0300

    NP-6: Connection treatment moved internal to SqlHelper

--- source/helpers/sql.helper.ts
+++ source/helpers/sql.helper.ts
@@ -6,7 +6,69 @@ import { ErrorHelper } from "./error.helper";
 export class SqlHelper {
     static sql: SqlClient = require("msnodesqlv8");
 
-    public static openConnection(): Promise<Connection> {
+    public static executeQueryArrayResult<T>(query: string): Promise<T[]> {
+        return new Promise<T[]>((resolve, reject) => {
+            SqlHelper.openConnection()
+                .then((connection: Connection) => {
+                    connection.query(query, (queryError: Error | undefined, queryResult: T[] | undefined) => {
+                        if (queryError) {
+                            reject(ErrorHelper.createError(ErrorCodes.QueryError, ErrorMessages.SqlQueryError));
+                        }
+                        else {
+                            if (queryResult !== undefined) {
+                                resolve(queryResult);
+                            }
+                            else {
+                                resolve([]);
+                            }
+                        }
+                    });
+                })
+                .catch((error: systemError) => {
+                    reject(error);
+                });
+        });
+    }
+
+    public static executeQuerySingleResult<T>(query: string): Promise<T> {
+        return new Promise<T>((resolve, reject) => {
+            SqlHelper.openConnection()
+                .then((connection: Connection) => {
+                    connection.query(query, (queryError: Error | undefined, queryResult: T[] | undefined) => {
+                        if (queryError) {
+                            reject(ErrorHelper.createError(ErrorCodes.QueryError, ErrorMessages.SqlQueryError));
+                        }
+                        else {
+                            const notFoundError: systemError = ErrorHelper.createError(ErrorCodes.NoData, ErrorMessages.NoDataFound);
+                    
+                            if (queryResult !== undefined) {
+                                switch (queryResult.length) {
+                                    case 0:
+                                        reject(notFoundError);
+                                        break;
+                            
+                                    case 1:
+                                        resolve(queryResult[0]);
+                                        break;
+                            
+                                    default: // In case more than a single result is returned
+                                        resolve(queryResult[0]);
+                                        break;
+                                }
+                            }
+                            else {
+                                reject(notFoundError);
+                            }
+                        }
+                    });
+                })
+                .catch((error: systemError) => {
+                    reject(error);
+                })
+        });
+    }
+
+    private static openConnection(): Promise<Connection> {
         return new Promise<Connection>((resolve, reject) => {
             SqlHelper.sql.open(DB_CONNECTION_STRING, (connectionError: Error, connection: Connection) => {
                 if (connectionError) {
@@ -14,56 +76,6 @@ export class SqlHelper {
                 }
                 else {
                     resolve(connection);
-                }
-            });
-        });
-    }
-
-    public static executeQueryArrayResult<T>(connection: Connection, query: string): Promise<T[]> {
-        return new Promise<T[]>((resolve, reject) => {
-            connection.query(query, (queryError: Error | undefined, queryResult: T[] | undefined) => {
-                if (queryError) {
-                    reject(ErrorHelper.createError(ErrorCodes.QueryError, ErrorMessages.SqlQueryError));
-                }
-                else {
-                    if (queryResult !== undefined) {
-                        resolve(queryResult);
-                    }
-                    else {
-                        resolve([]);
-                    }
-                }
-            });
-        });
-    }
-
-    public static executeQuerySingleResult<T>(connection: Connection, query: string): Promise<T> {
-        return new Promise<T>((resolve, reject) => {
-            connection.query(query, (queryError: Error | undefined, queryResult: T[] | undefined) => {
-                if (queryError) {
-                    reject(ErrorHelper.createError(ErrorCodes.QueryError, ErrorMessages.SqlQueryError));
-                }
-                else {
-                    const notFoundError: systemError = ErrorHelper.createError(ErrorCodes.NoData, ErrorMessages.NoDataFound);
-                    
-                    if (queryResult !== undefined) {
-                        switch (queryResult.length) {
-                            case 0:
-                                reject(notFoundError);
-                                break;
-                            
-                            case 1:
-                                resolve(queryResult[0]);
-                                break;
-                            
-                            default: // In case more than a single result is returned
-                                resolve(queryResult[0]);
-                                break;
-                        }
-                    }
-                    else {
-                        reject(notFoundError);
-                    }
                 }
             });
         });
