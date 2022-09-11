cc391422b7a57c227b384348bbbc390ce702e579
Ilya Pogrebensky <ilya@billboy.co.il>
2022-09-11 16:10:09 +0300

    NP-6: Refactor of Request parse of numeric input added

--- source/controllers/school.controller.ts
+++ source/controllers/school.controller.ts
@@ -2,6 +2,7 @@ import { Request, Response, NextFunction } from 'express';
 import { ErrorCodes, ErrorMessages } from '../constants';
 import { systemError, whiteBoardType } from '../entities';
 import { ErrorHelper } from '../helpers/error.helper';
+import { RequestHelper } from '../helpers/request.helper';
 import { ResponseHelper } from '../helpers/response.helper';
 import { SchoolService } from '../services/school.service';
 
@@ -20,6 +21,27 @@ const getBoardTypes = async (req: Request, res: Response, next: NextFunction) =>
 };
 
 const getBoardTypeById = async (req: Request, res: Response, next: NextFunction) => {
+    const numericParamOrError: number | systemError = RequestHelper.ParseNumericInput(req.params.id)
+    if (typeof numericParamOrError === "number") {
+        if (numericParamOrError > 0) {
+            schoolService.getBoardTypeById(numericParamOrError)
+                .then((result: whiteBoardType) => {
+                    return res.status(200).json(result);
+                })
+                .catch((error: systemError) => {
+                    return ResponseHelper.handleError(res, error);
+                });
+        }
+        else {
+            // TODO: Error handling
+        }
+    }
+    else {
+        return ResponseHelper.handleError(res, numericParamOrError);
+    }
+};
+
+const updateBoardTypeById = async (req: Request, res: Response, next: NextFunction) => {
     let id: number = -1;
 
     const sId: string = req.params.id;
@@ -65,4 +87,4 @@ const getBoardTypeByTitle = async (req: Request, res: Response, next: NextFuncti
         });
 };
 
-export default { getBoardTypes, getBoardTypeById, getBoardTypeByTitle };
+export default { getBoardTypes, getBoardTypeById, getBoardTypeByTitle, updateBoardTypeById };
---
+++ source/helpers/request.helper.ts
@@ -0,0 +1,23 @@
+import { ErrorCodes, ErrorMessages } from "../constants";
+import { systemError } from "../entities";
+import { ErrorHelper } from "./error.helper";
+
+export class RequestHelper {
+
+    public static ParseNumericInput(input: string): number | systemError {
+        let result: number = -1;
+
+        if (isNaN(Number(input))) {
+            return ErrorHelper.createError(ErrorCodes.NonNumericInput, ErrorMessages.NonNumericInput);
+        }
+
+        if (input !== null && input !== undefined) {
+            result = parseInt(input);
+        }
+        else {
+            return ErrorHelper.createError(ErrorCodes.InputParameterNotSupplied, ErrorMessages.InputParameterNotSupplied);
+        }
+
+        return result;
+    }
+}
