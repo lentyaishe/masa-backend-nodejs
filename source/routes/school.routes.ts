ad6db16a13b0e8664e41058b67293ff1a37dec17
Ilya Pogrebensky <ilya@billboy.co.il>
2022-09-11 16:10:09 +0300

    NP-6: Refactor of Request parse of numeric input added

--- source/routes/school.routes.ts
+++ source/routes/school.routes.ts
@@ -5,5 +5,6 @@ const router = express.Router();
 router.get('/general/board-types', controller.getBoardTypes);
 router.get('/general/board-type/:id', controller.getBoardTypeById);
 router.get('/general/board-type-by-title/:title', controller.getBoardTypeByTitle);
+router.put('/general/board-type/:id', controller.updateBoardTypeById);
 
 export default { router };
