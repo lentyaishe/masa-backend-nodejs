7ce3b1046e01fe7afda5aedd02b3b873ebc8a36f
Ilya Pogrebensky <ilya@billboy.co.il>
2022-09-18 11:08:20 +0300

    NP-15 complete: Refactor whit board type to support data basic information

--- source/constants.ts
+++ source/constants.ts
@@ -30,7 +30,7 @@ export class Queries {
     public static WhiteBoardTypeById: string = `SELECT * FROM white_board_type WHERE id = ? AND status_id = ?`;
     public static WhiteBoardTypeByTitle: string = "SELECT * FROM white_board_type WHERE white_board_type LIKE ?";
     public static UpdateWhiteBoardTypeById: string = "UPDATE white_board_type SET white_board_type = ?, update_date = ?, update_user_id = ? WHERE id = ? AND status_id = ?";
-    public static AddWhiteBoardType: string = "INSERT white_board_type (white_board_type) VALUES (?)";
+    public static AddWhiteBoardType: string = "INSERT white_board_type (white_board_type, create_date, update_date, create_user_id, update_user_id, status_id) VALUES (?, ?, ?, ?, ?, ?)";
     public static DeleteWhiteBoardTypeById: string = "UPDATE white_board_type SET update_date = ?, update_user_id = ?, status_id = ? WHERE id = ? AND status_id = ?";
 }
 
