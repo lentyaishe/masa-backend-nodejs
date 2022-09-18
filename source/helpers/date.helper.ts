dfec7df51f4317e45bc756f9314838f62de85acc
Ilya Pogrebensky <ilya@billboy.co.il>
2022-09-18 10:45:30 +0300

    NP-15: DELETE aligned

--- source/helpers/date.helper.ts
+++ source/helpers/date.helper.ts
@@ -8,6 +8,6 @@ export class DateHelper {
         const minute: string = (input.getMinutes()).toString().padStart(2, "0");
         const second: string = (input.getSeconds()).toString().padStart(2, "0");
 
-        return `${year}${month}${day} ${hour}${minute}${second}`;
+        return `${year}${month}${day} ${hour}:${minute}:${second}`;
     }
 }
