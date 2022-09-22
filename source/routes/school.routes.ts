af6669f87b9d063a2cb2ee4defebc0b8c11bee77
Ilya Pogrebensky <ilya@billboy.co.il>
2022-09-22 16:44:50 +0300

    NP-18 complete: Add authorization by roles

--- source/routes/school.routes.ts
+++ source/routes/school.routes.ts
@@ -1,13 +1,14 @@
 import express from 'express';
 import controller from '../controllers/school.controller';
+import { Role } from '../enums';
 import middleware from '../middleware/auth.middleware';
 const router = express.Router();
 
-router.get('/board-types', middleware.verifyToken, controller.getBoardTypes);
+router.get('/board-types', middleware.verifyToken([Role.Administrator, Role.RegularUser]), controller.getBoardTypes);
 router.get('/board-type/:id', controller.getBoardTypeById);
 router.get('/board-type-by-title/:title', controller.getBoardTypeByTitle);
-router.put('/board-type/:id', middleware.verifyToken, controller.updateBoardTypeById);
-router.post('/board-type', middleware.verifyToken, controller.addBoardType);
-router.delete('/board-type/:id', middleware.verifyToken, controller.deleteBoardTypeById);
+router.put('/board-type/:id', middleware.verifyToken([Role.Administrator]), controller.updateBoardTypeById);
+router.post('/board-type', middleware.verifyToken([Role.Administrator]), controller.addBoardType);
+router.delete('/board-type/:id', middleware.verifyToken([Role.Administrator]), controller.deleteBoardTypeById);
 
 export default { router };
