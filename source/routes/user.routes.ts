406a0b2bc887ada25e70c9d5ff8e8a717c5e9047
Ilya Pogrebensky <ilya@billboy.co.il>
2022-09-22 17:42:15 +0300

    NP-21 complete: Add user CRUD

---
+++ source/routes/user.routes.ts
@@ -0,0 +1,11 @@
+import express from 'express';
+import controller from '../controllers/user.controller';
+import { Role } from '../enums';
+import middleware from '../middleware/auth.middleware';
+const router = express.Router();
+
+router.post('/', middleware.verifyToken([Role.Administrator]), controller.add);
+router.put('/:id', middleware.verifyToken([Role.Administrator]), controller.updateById);
+router.delete('/:id', middleware.verifyToken([Role.Administrator]), controller.deleteById);
+
+export default { router };
