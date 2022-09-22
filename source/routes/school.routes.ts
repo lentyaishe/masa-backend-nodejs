a9f895d642cda9ccbbaddc4b04405fe4d016244b
Ilya Pogrebensky <ilya@billboy.co.il>
2022-09-22 16:57:27 +0300

    NP-19 complete: Refactor authentication token

--- source/routes/school.routes.ts
+++ source/routes/school.routes.ts
@@ -5,10 +5,10 @@ import middleware from '../middleware/auth.middleware';
 const router = express.Router();
 
 router.get('/board-types', middleware.verifyToken([Role.Administrator, Role.RegularUser]), controller.getBoardTypes);
-router.get('/board-type/:id', controller.getBoardTypeById);
-router.get('/board-type-by-title/:title', controller.getBoardTypeByTitle);
-router.put('/board-type/:id', middleware.verifyToken([Role.Administrator]), controller.updateBoardTypeById);
-router.post('/board-type', middleware.verifyToken([Role.Administrator]), controller.addBoardType);
-router.delete('/board-type/:id', middleware.verifyToken([Role.Administrator]), controller.deleteBoardTypeById);
+router.get('/board-type/:id', middleware.verifyToken([Role.Administrator, Role.RegularUser]), controller.getBoardTypeById);
+router.get('/board-type-by-title/:title', middleware.verifyToken([Role.Administrator, Role.RegularUser]), controller.getBoardTypeByTitle);
+router.put('/board-type/:id', middleware.verifyToken([Role.Administrator, Role.RegularUser]), controller.updateBoardTypeById);
+router.post('/board-type', middleware.verifyToken([Role.Administrator, Role.RegularUser]), controller.addBoardType);
+router.delete('/board-type/:id', middleware.verifyToken([Role.Administrator, Role.RegularUser]), controller.deleteBoardTypeById);
 
 export default { router };
